98d7670fb6b38aaf4c303f536d1277369b26f17c
axisd <lordrra@gmail.com>
2024-06-25 21:17:06 +0300

    add bloooooooooooood fix bite (repeat) fix rotate after chase

--- Scenes/Monsters/FuuMonster.gd
+++ Scenes/Monsters/FuuMonster.gd
@@ -1,12 +1,14 @@
 extends CharacterBody3D
 
 @export var speed : float = 3.0
+@export var turn_speed : float = 2.0
 
 var current_state: String = "idle"
 var next_state : String = "idle"
 var previous_state : String
 
 @onready var player : CharacterBody3D
+@onready var blood_spray : PackedScene = preload("res://Scenes/Monsters/blood_spray_enemy.tscn")
 
 func _ready():
 	$MonsterBody/AnimationPlayer.play("Rest")
@@ -37,6 +39,12 @@ func chase(delta : float):
 	
 	velocity = ($NavigationAgent3D.get_next_path_position() - position).normalized() * speed * delta
 	
+	$FaceDirection.look_at(player.position, Vector3.UP)
+	rotate_y(deg_to_rad(-$FaceDirection.rotation.y * turn_speed))
+	
+	if player.position.distance_to(self.position) < 2:
+		next_state = "bite"
+		
 	if player.position.distance_to(self.position) > 1:
 		$NavigationAgent3D.target_position = player.position
 		move_and_collide(velocity)
@@ -48,18 +56,10 @@ func bite():
 
 func take_damage(damage : int):
 	print("fuu take damage: ", damage)
+	var blood : Node3D = blood_spray.instantiate()
+	self.add_child(blood)
 
 # slots
-func _on_area_3d_body_entered(body):
-	if body.is_in_group("player"):
-		next_state = "bite"
-
-
-func _on_area_3d_body_exited(body):
-	if body.is_in_group("player"):
-		next_state = "idle"
-
-
 func _on_aggro_area_3d_body_entered(body):
 	if body.is_in_group("player"):
 		next_state = "chase"
@@ -68,3 +68,9 @@ func _on_aggro_area_3d_body_entered(body):
 func _on_aggro_area_3d_body_exited(body):
 	if body.is_in_group("player"):
 		next_state = "idle"
+
+
+func _on_animation_player_animation_finished(anim_name):
+	match anim_name:
+		"Bite":
+			next_state = "chase"
